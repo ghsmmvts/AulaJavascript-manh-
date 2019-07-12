a9fb6f12774e7dc85df2ce03d7288685f02ef136
guilherme henrique <ghsmmvts@gmail.com>
2019-07-12 09:43:28 -0300

    aula sobre for e laços

---
+++ scriptfor.js
@@ -0,0 +1,80 @@
+// let nome = prompt('Digite seu nome');
+// let idade = prompt('Digite sua idade');
+
+// // let x = idade > 30 'Sim' 'Não'
+
+// if(idade>= 30){
+//     alert('Seu velho');
+// }
+// else if(idade >=25){
+//     alert('Tá chegando lá');
+// }
+// else {
+//     alert('novinho');
+// }
+
+// *************************************************************
+
+// let nome = prompt('Digite seu nome');
+// let idade = prompt('Digite sua idade');
+
+// // let frase;
+
+// // let x = idade > 30 'Sim' 'Não'
+
+// // if(idade>= 30){
+// //     frase('Seu velho');
+// // }
+// // else {
+// //     frase('novinho');
+// // }
+
+// let frase = idade >= 30 ? 'Seu véio' : 'Novinho';
+
+// alert(frase);
+
+// *********************************************************************
+
+// LAÇO
+
+// let contador = 1;
+
+// do{
+//     console.log(contador);
+//     contador++;
+// }while(contador <= 1000);
+
+// while (contador <= 1000){
+//     console.log(contador);
+//     contador++;
+// }
+
+// **********************************************************************
+
+// Laços for
+
+// for(let i = 0; i < 100; i++){
+//     console.log(i);
+// }
+
+// for(let i = 99; i > 0; i--){
+//     console.log(i);
+// }
+
+// FAZER COM Números Pares
+
+// for (let i = 2; i <= 100; i += 2){
+//     console.log(i);
+// }
+
+// for(let i = 2; i <=  100; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 2; i <= 100; i++){
+//     console.log(i);
+//     i++;
+// }
+
